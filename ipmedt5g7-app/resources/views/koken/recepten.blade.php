53aca7c6ffb73427205398c00f3adbaf7e31f09b
Burak-Kivrak <s1117029@student.hsleiden.nl>
2021-03-24 10:41:33 +0100

    afbeelding bug gefixt

--- ipmedt5g7-app/resources/views/koken/recepten.blade.php
+++ ipmedt5g7-app/resources/views/koken/recepten.blade.php
@@ -7,18 +7,16 @@
 @section('content')
     <ul class='u-grid-12 u-grid-gap-2'>
         @foreach($recepten as $recepten)
-            <li class='receptKaart u-list-style-none'>
-                <article>
-                    <header class='receptKaart__header'>  
+            <li class='receptKaart u-list-style-none'> 
+                    <header class='receptKaart__header u-flex-v-center'>  
                         <h2 class='receptKaart__heading'> {{$recepten->naam}} </h2>
                     </header>
                     <figure class='receptKaart__figure'>
-                        <img class='receptKaart_img' src="{{$recepten->afbeelding}}" alt="{{$recepten->afbeelding}}">
+                        <img class='receptKaart__img' src="{{$recepten->afbeelding}}" alt="{{$recepten->afbeelding}}">
                     </figure>
-                    <section class='receptKaart_btnSection'>
-                        <button class='receptKaart_button'> Lees meer </button>
+                    <section class='receptKaart__btnSection u-flex-v-center'>
+                        <button class='receptKaart__button'> Lees meer </button>
                     </section>
-                </article>
             </li>
         @endforeach
     </ul>
