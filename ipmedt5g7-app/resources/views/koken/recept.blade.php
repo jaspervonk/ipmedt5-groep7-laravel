d58c9901ea7253dce5a6b7b6f50100767f0bcb1a
Burak-Kivrak <s1117029@student.hsleiden.nl>
2021-03-29 23:39:00 +0200

    recept css toegepast + kleurenpalette uitgetest

--- ipmedt5g7-app/resources/views/koken/recept.blade.php
+++ ipmedt5g7-app/resources/views/koken/recept.blade.php
@@ -2,23 +2,32 @@
 
 @section('content')
 <article class='recept'>
-    <header class='recept__header'>
+    <header class='recept__header u-flex-v-center'>
         <h1 class='recept__heading'>{{$recepten->naam}}</h1>
     </header>
-    <figure class='recept__figure'>
-        <img class='recept__img' src="{{$recepten->afbeelding}}" alt="{{$recepten->afbeelding}}">
-    </figure>
-    <section class='recept__informatie'>
-        <p class='recept__informatie__tekst'> Bereiden voor: 2 personen </p>
-        <p class='recept__informatie__tekst'> Duur: 20 minuten </p>   
+    <section class='recept__section1'>
+        <figure class='recept__figure'>
+            <img class='recept__img' src="{{$recepten->afbeelding}}" alt="{{$recepten->afbeelding}}">
+        </figure>
+        <section class='recept__informatie'>
+            <section class='recept__informatie__kaart'>
+                <p class='recept__informatie__tekst'> Bereiden voor: 2 pers. </p>
+                <p class='recept__informatie__tekst'> Duur: 20 minuten </p>
+                <p class='recept__informatie__tekst'> Te behalen punten: 3</p>   
+            </section>
+        </section>
     </section>
-    <section class='recept__ingredienten'>
-        <h2 class='recept__ingredienten__heading'> Ingrediënten </h2>
-        <p class='recept__ingredienten__tekst'> {!! nl2br(e($recepten->ingredienten)) !!} </p>
-    </section>
-    <section class='recept__bereiden'>
-        <h2 class='recept__ingredienten__heading'> Hoe te bereiden </h2>
-        <p class='recept__bereiden__tekst'> {{$recepten->bereiden}} </p>
+    <section class=recept__section2>
+        <section class='recept__bereiden'>
+            <h2 class='recept__bereiden__heading'> Hoe te bereiden </h2>
+            <p class='recept__bereiden__tekst'> {{$recepten->bereiden}} </p>
+        </section>
+        <section class='recept__ingredienten'>
+            <section class='recept__ingredienten__kaart'>
+                <h2 class='recept__ingredienten__heading'> Ingrediënten </h2>
+                <p class='recept__ingredienten__tekst'> {!! nl2br(e($recepten->ingredienten)) !!} </p>
+            </section>
+        </section>
     </section>
 </article>
 
