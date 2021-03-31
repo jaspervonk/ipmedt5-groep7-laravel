b3c34ba89af76dfaeef52ee19802981737fe4103
s1121610 <s1121610@student.hsleiden.nl>
2021-03-31 13:15:42 +0200

    Composer autoload

--- ipmedt5g7-app/app/Http/Controllers/ApiController.php
+++ ipmedt5g7-app/app/Http/Controllers/ApiController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 // use \GuzzleHttp\Client;
-use Illuminate\Http\Client\Response;
+// use Illuminate\Http\Client\Response;
 use \App\Models\StoredProducts;
-use ArrayAccess;
-use DOMDocument;
-use \App\Goutte\Client;
+// use ArrayAccess;
+// use DOMDocument;
+// use \App\Goutte\Client;
 
 
 class ApiController extends Controller
