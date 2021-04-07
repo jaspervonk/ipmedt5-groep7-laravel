5423641aa8ef4ab5bd0c1cb9e148c5d8c53a5e8b
s1121610 <s1121610@student.hsleiden.nl>
2021-04-07 16:14:54 +0200

    update

--- ipmedt5g7-app/resources/views/boodschappen/changeUser.blade.php
+++ ipmedt5g7-app/resources/views/boodschappen/changeUser.blade.php
@@ -1,10 +1,10 @@
 <form action="/changeUser" method="POST">
     @csrf
     <label class="blackFont" for="naam">Selecteer lijstje: </label>
-    <select name="naam">
+    <select name="naam" onchange="this.form.submit()">
         @foreach($user as $user)
             <option value="{{$user->name}}">{{$user->name}}</option>
         @endforeach
     </select>
-    <button class="button button--red button--small" type="submit">Veranderen</button>
+    <!-- <button class="button button--red button--small" type="submit">Veranderen</button> -->
 </form>
