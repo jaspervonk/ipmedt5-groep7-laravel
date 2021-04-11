d1b75d695c2352537289b880044a6b54d2ba8e23
jaspervonk <s1122268@student.hsleiden.nl>
2021-04-11 16:20:18 +0200

    Gebruikers groet toegevoegd

--- ipmedt5g7-app/resources/views/gebruiker/gebruiker.blade.php
+++ ipmedt5g7-app/resources/views/gebruiker/gebruiker.blade.php
@@ -15,7 +15,7 @@
     </header>
     <article class='gebruiker u-box-shadow-content'>
         @foreach($ActiveUser as $ActiveUser)
-            <h3 class="gebruiker__header__text u-box-shadow-content"> {{$ActiveUser->name}}</h3>
+            <h3 class="gebruiker__header__text u-box-shadow-content"> Hallo {{$ActiveUser->name}}!</h3>
         @endforeach
         <section class='gebruiker__section1'>
             <h3 class='gebruiker__title'>Gebruiker aanmaken</h3>
@@ -24,7 +24,7 @@
                 {{ csrf_field() }}
                 <label for="name">Naam:</label><br>
                 <section class='gebruiker__flex'>
-                    <input class='gebruiker__input' type="text" name="name"><br>
+                    <input class='gebruiker__input' type="text" name="name" required><br>
                     <button class="gebruiker__button" type="submit">Aanmaken</button>
                 </section>
             </form>
