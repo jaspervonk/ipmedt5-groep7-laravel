126a4f717c5991a19ed3f47cae6510dd10224d5c
s1121610 <s1121610@student.hsleiden.nl>
2021-04-16 12:14:46 +0200

    grid anders ingedeeld om enorm stuk witte pagina onderaan boodschappenpagina weg te werken.

--- ipmedt5g7-app/resources/views/boodschappen/form.blade.php
+++ ipmedt5g7-app/resources/views/boodschappen/form.blade.php
@@ -1,5 +1,5 @@
 <section class="card u-box-shadow grid__toevoegen">
-    <h2>Toevoegen aan database:</h2>
+    <h2>Toevoegen aan database</h2>
     <hr class="u-stripe">
     <form action="/boodschappenlijst/store" method="POST" onsubmit='return setPopup("Product is toegevoegd aan de database.", "linear-gradient(180deg, #5ACD34 0%, #2B8D29 100%)");'>
         @method('POST')
@@ -21,7 +21,7 @@
     </form>
 </section>
 <section class="card u-box-shadow grid__barcodeScannen">
-    <h2>Product toevoegen:</h2>
+    <h2>Product toevoegen</h2>
     <hr class="u-stripe">
     <form action="/boodschappenlijst/api" method="POST" onsubmit='return setPopup("Product is toegevoegd.", "linear-gradient(180deg, #5ACD34 0%, #2B8D29 100%)");'>
         @method('POST')
